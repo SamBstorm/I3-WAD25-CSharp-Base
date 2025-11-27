037baebfc8965821cc29e08945d9b7bf12bc4398
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-27 14:26:26 +0100

    Add Exo03

---
+++ Exo03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exo03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Veuillez entrer un premier nombre : ");
+            int nombre1 = int.Parse(Console.ReadLine()!);
+            Console.Write("Veuillez entrer un deuxième nombre : ");
+            int nombre2 = int.Parse(Console.ReadLine()!);
+
+            int div = nombre1 / nombre2;
+            int modulo = nombre1 % nombre2;
+            double division = (double)nombre1 / nombre2;
+
+            Console.WriteLine($"Divsion entière : {div} ; Modulo : {modulo} ; Division classique : {division}");
+        }
+    }
+}
