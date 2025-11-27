6be89c427ab09704ece2119bf658e39508ae6736
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-27 14:32:30 +0100

    Add Demo06 - Substring

---
+++ Demo06/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "012345678910";
+
+            string word1 = message.Substring(0, 5);
+            string word2 = message.Substring(7);
+
+            Console.WriteLine(word1);
+            Console.WriteLine(word2);
+        }
+    }
+}
