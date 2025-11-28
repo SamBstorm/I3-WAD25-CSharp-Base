8dcb06a15ff57f13bbd65fd7292ef15407ad4316
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-28 15:08:10 +0100

    Add Exo08

---
+++ Exo08/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exo08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint fibonacci = 25;
+
+            uint a = 0;
+            uint b = 1;
+
+            Console.WriteLine($"Suite de Fibonacci jusqu'à {fibonacci} :");
+
+            for (uint i = 0; i <= fibonacci; i++)
+            {
+                Console.Write(a + " ");
+                uint next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+    }
+}
