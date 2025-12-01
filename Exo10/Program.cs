6a7f15e734cb24ba27ac5274442bb2185a46c147
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-01 10:13:19 +0100

    Add Exo10

---
+++ Exo10/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exo10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort nombre;
+
+            do
+            {
+                Console.Write("Bonjour, veuillez indiquer le nombre de nombres premiers que vous voulez afficher : ");                 
+            } while (!ushort.TryParse(Console.ReadLine(), out nombre) || nombre <= 0);
+
+
+            for (int nbATester = 2, compteurNbPremier = 0; compteurNbPremier < nombre; nbATester++)
+            {
+                bool estPremier = true;                
+
+                for (int diviseur = 2; diviseur <= Math.Sqrt(nbATester) && estPremier; diviseur++)
+                {
+                    if (nbATester % diviseur == 0) estPremier = false;
+                }
+
+                if (estPremier)
+                {
+                    Console.WriteLine($"Le nombre {nbATester} est un nombre premier!");
+                    compteurNbPremier++;
+                }
+            }
+        }
+    }
+}
