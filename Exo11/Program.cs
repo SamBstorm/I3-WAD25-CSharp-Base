514a93169f47608e25b1936fcfec550ab19c71e4
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-03 10:04:03 +0100

    Add Exo11 : Version 1

---
+++ Exo11/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exo11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] tab = new char[10];
+
+            int position = 0;
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = '-';
+            }
+
+            tab[position] = 'o';
+            ConsoleKey lettre;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Appuyez sur 'D' pour aller à droite ou 'G' pour aller à gauche");
+                Console.WriteLine(tab);
+                lettre = Console.ReadKey(true).Key;
+                tab[position] = '-';
+                switch (lettre)
+                {
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
+                        if (position < tab.Length - 1)
+                        {
+                            position++;
+                        }
+                        break;
+                    case ConsoleKey.G:
+                    case ConsoleKey.LeftArrow:
+                        if (position > 0)
+                        {
+                            position--;
+                        }
+                        break;
+                }
+                tab[position] = 'o';
+            } while (lettre != ConsoleKey.Escape);
+        }
+    }
+}
