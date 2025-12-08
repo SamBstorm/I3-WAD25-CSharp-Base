a6245ab61ee6608b4a32e11e628c5fdccadb8cca
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-08 11:52:59 +0100

    Add Exo14

---
+++ Exo14/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace Exo14
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
+}
---
+++ Exo14/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exo14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const ushort TAILLE_GRILLE = 5;
+            Point?[,] grille = new Point?[TAILLE_GRILLE, TAILLE_GRILLE];
+
+            for (int i = 0; i < TAILLE_GRILLE; i++)
+            {
+                Point p;
+                p.X = i + 1;
+                p.Y = i + 1;
+                grille[i, i] = p;
+                /* OU
+                grille[i,i] = new Point() { X = i + 1, Y = i + 1 };*/
+            }
+
+            for (int i = 0; i < TAILLE_GRILLE; i++)
+            {
+                for(int j = 0; j < TAILLE_GRILLE; j++)
+                {
+                    if (grille[i,j] is null)
+                    {
+                        Console.Write("\t");
+                    }
+                    else
+                    {
+                        Point p = (Point)grille[i, j];
+                        Console.Write($"X : {p.X} - Y : {p.Y}");
+                        /*OU
+                        Console.Write($"X : {grille[i, j]?.X} - Y : {grille[i, j]?.Y}");*/
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
