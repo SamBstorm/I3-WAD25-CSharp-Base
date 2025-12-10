563590d1a9ca04f4d9075f9ea156008a3bfc4c33
Samuel Legrain <samuel.legrain@bstorm.be>
2025-12-10 10:17:53 +0100

    Add Exo17

---
+++ Exo17/Menu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo17
+{
+    public struct Menu
+    {
+        public string message;
+        public string[] choix;
+        public ushort position;
+
+        public void Monter()
+        {
+            if(position == 0)
+            {
+                position = (ushort)(choix.Length - 1);
+            }
+            else 
+            { 
+                position--; 
+            }
+            //position = (position == 0) ? (ushort)(choix.Length - 1) : (ushort)(position - 1);
+        }
+
+        public void Descendre()
+        {
+            if (position == choix.Length - 1)
+            {
+                position = 0;
+            }
+            else { 
+                position++;
+            }
+        }
+
+        public ushort Afficher()
+        {
+            ConsoleKey key;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                Console.WriteLine();
+                for (int i = 0; i < choix.Length; i++)
+                {
+                    if(i == position)
+                    {
+                        Console.WriteLine($"-> {choix[i]} <-");
+                    }
+                    else
+                    {
+                        Console.WriteLine(choix[i]);
+                    }
+                }
+                do
+                {
+                    key = Console.ReadKey(true).Key;
+                } while (key != ConsoleKey.Enter && key != ConsoleKey.DownArrow && key != ConsoleKey.UpArrow);
+                if(key == ConsoleKey.UpArrow)
+                {
+                    Monter();
+                }
+                else if(key == ConsoleKey.DownArrow)
+                {
+                    Descendre();
+                }
+            }
+            while (key != ConsoleKey.Enter);
+            return position;
+        }
+    }
+}
---
+++ Exo17/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exo17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menuPrincipal = new Menu();
+            menuPrincipal.message = "Menu Principal";
+            menuPrincipal.choix = new string[] { "Jouer", "Continuer", "Options", "Quitter" };
+
+            ushort choix = menuPrincipal.Afficher();
+
+            switch (choix)
+            {
+                case 0:
+                    Console.WriteLine("Commençons une partie!");
+                    Menu chapitre1 = new Menu();
+                    chapitre1.message = "Vous entrez dans un sombre manoir, que faites-vous?";
+                    chapitre1.choix = ["Demi-tour!", "On avance...", "On hurle!"];
+                    choix = chapitre1.Afficher();
+                    switch (choix)
+                    {
+                        case 0:
+                            Console.WriteLine("Bravo, vous êtes le seul survivant!");
+                            break;
+                        case 1:
+                            Console.WriteLine("Pour avancer plus loin, veuillez payer l'éditeur du jeu...");
+                            break;
+                        default:
+                            Console.WriteLine("Un fantôme vous surprend, vous mourrez...");
+                            break;
+                    }
+                    break;
+                case 1:
+                    Console.WriteLine("Où en étions-nous ?");
+                    break;
+                case 2:
+                    Console.WriteLine("Réglons ça!");
+                    break;
+                default:
+                    Console.WriteLine("Au revoir!");
+                    break;
+            }
+        }
+    }
+}
